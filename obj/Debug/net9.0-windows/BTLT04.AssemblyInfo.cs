e7c946e39ca68a4d001f92136c256309fd5214f6
Dat-se40 <tandatthtt@gmail.com>
2025-11-15 14:50:01 +0700

    11/15

--- obj/Debug/net9.0-windows/BTLT04.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/BTLT04.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BTLT04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325e661c62a1eb9796eeff5ab689cf5c03eeff9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5782cd2581fff380003b042f5544781ee3120d11")]
 [assembly: System.Reflection.AssemblyProductAttribute("BTLT04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BTLT04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
